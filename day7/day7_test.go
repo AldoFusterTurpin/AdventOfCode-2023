de260b431e5fc706610019c105b0dda0b6b90dc9
Aldo <aldoft@hotmail.com>
2024-04-13 15:30:49 +0200

    day 7, part 1: add test for NewHand

--- day7/day7_test.go
+++ day7/day7_test.go
@@ -1,23 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"strconv"
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestNewHand(t *testing.T) {
 	type testCase struct {
+		cards        string
+		wantHandType HandType
 	}
-
-	testCases := []testCase{
-		{},
+	tests := []testCase{
+		{
+			cards:        "32T3K",
+			wantHandType: OnePair,
+		},
+		{
+			cards:        "T55J5",
+			wantHandType: ThreeOfAkind,
+		},
+		{
+			cards:        "KK677",
+			wantHandType: TwoPair,
+		},
+		{
+			cards:        "KTJJT",
+			wantHandType: TwoPair,
+		},
+		{
+			cards:        "QQQJA",
+			wantHandType: ThreeOfAkind,
+		},
 	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			hand := NewHand(tt.cards)
+			handType := hand.handType
 
-	for i, tc := range testCases {
-		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
-			got := fn()
-			if got != tc.expected {
-				t.Fatalf("expected %v, but got %v", tc.expected, got)
+			if handType != tt.wantHandType {
+				t.Errorf("expected %v, but got %v", tt.wantHandType, handType)
 			}
 		})
 	}
