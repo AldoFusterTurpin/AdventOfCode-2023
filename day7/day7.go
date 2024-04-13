c0df94d5bccaae27a5ff3753bc4412b865dc6f6e
Aldo <aldoft@hotmail.com>
2024-04-13 20:18:43 +0200

    day 7, part 1: create sortByStrength() and make the test pass

--- day7/day7.go
+++ day7/day7.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
+	"sort"
 	"strconv"
 	"strings"
 )
+
+// A hand consists of five cards labeled one of A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2.
+// The relative strength of each card follows this order, where A is the highest and 2 is the lowest.
+var cardsSortedByStrength = []rune{'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'}
 
 type HandType string
 
@@ -82,6 +88,10 @@ type HandWithBid struct {
 	bid  int
 }
 
+func sortByStrength(hs []HandWithBid) {
+	sort.Sort(ByStrength(hs))
+}
+
 func NewHandWithBid(cards, bid string) (*HandWithBid, error) {
 	bidInt, err := strconv.Atoi(bid)
 	if err != nil {
@@ -94,6 +104,96 @@ func NewHandWithBid(cards, bid string) (*HandWithBid, error) {
 	}
 
 	return handWithBid, nil
+}
+
+type ByStrength []HandWithBid
+
+func (o ByStrength) Len() int {
+	return len(o)
+}
+
+func (o ByStrength) Swap(i, j int) {
+	o[i], o[j] = o[j], o[i]
+}
+
+// Less implements the function to order the []HandWithBid based on the rank,
+// where the weakest hand gets rank 1, the second-weakest hand gets rank 2,
+// and so on up to the strongest hand. So Less should return true when card[i] is weaker than card[j]
+func (b ByStrength) Less(left, right int) bool {
+	// return true when let is weaker than right
+	// I know this can be refactored to use indexes for the type of Hand and just compare the order of
+	// the indexes instead of comparing that much cases. I want the test green first :)
+	leftHandType := b[left].hand.handType
+	rightHandType := b[right].hand.handType
+
+	if leftHandType == rightHandType {
+		return b.isLeftHandWeakerThanRightHand(left, right)
+	}
+	if rightHandType == FiveOfAKind {
+		return true
+	}
+	if leftHandType == HighCard {
+		return true
+	}
+	if leftHandType == OnePair && rightHandType != HighCard {
+		return true
+	}
+	if leftHandType == TwoPair && rightHandType != OnePair && rightHandType != HighCard {
+		return true
+	}
+	if leftHandType == ThreeOfAkind && rightHandType != TwoPair && rightHandType != OnePair && rightHandType != HighCard {
+		return true
+	}
+	if leftHandType == FullHouse && rightHandType != ThreeOfAkind && rightHandType != TwoPair && rightHandType != OnePair && rightHandType != HighCard {
+		return true
+	}
+	if leftHandType == FourOfAkind && rightHandType != FullHouse && rightHandType != ThreeOfAkind && rightHandType != TwoPair && rightHandType != OnePair && rightHandType != HighCard {
+		return true
+	}
+	return false
+}
+
+func (b ByStrength) isLeftHandWeakerThanRightHand(left, right int) bool {
+	// If two hands have the same type, a second ordering rule takes effect.
+	// Start by comparing the first card in each hand.
+	// If these cards are different, the hand with the stronger first card is considered stronger.
+	// If the first card in each hand have the same label, however, then move on to considering the second card in each hand.
+
+	nCardsLeft := len(b[left].hand.cards)
+	nCardsRight := len(b[right].hand.cards)
+
+	// this should never happen
+	if nCardsLeft != nCardsRight {
+		log.Fatal("error, not the same number of cards in left hand and right hand")
+	}
+
+	for index := 0; index < nCardsLeft; index++ {
+		leftCard := b[left].hand.cards[index]
+		rightCard := b[right].hand.cards[index]
+		if leftCard == rightCard {
+			continue
+		}
+		return isLeftCardWeakerThanRightCard(rune(leftCard), rune(rightCard))
+	}
+
+	// according to the problem statement, we should never reach this point
+	log.Fatalf("error, %v appears twice", b[left])
+	return false
+}
+
+// isLeftCardWeakerThanRightCard returns true if leftCard is stronger than rightCard, otherwise returns false.
+func isLeftCardWeakerThanRightCard(leftCard, rightCard rune) bool {
+	leftCardIndex := slices.Index(cardsSortedByStrength, leftCard)
+	if leftCardIndex == -1 {
+		log.Fatalf("leftCard %v is not present in cardsSortedByStrength", string(leftCard))
+	}
+
+	rightCardIndex := slices.Index(cardsSortedByStrength, rightCard)
+	if rightCardIndex == -1 {
+		log.Fatalf("rightCard %v is not present in cardsSortedByStrength", string(rightCard))
+	}
+
+	return leftCardIndex > rightCardIndex
 }
 
 func main() {
--- day7/day7_test.go
+++ day7/day7_test.go
@@ -120,3 +120,98 @@ QQQJA 483`,
 		})
 	}
 }
+
+func Test_orderByStrength(t *testing.T) {
+	type testCase struct {
+		handWithBid     []HandWithBid
+		wantHandWithBid []HandWithBid
+	}
+
+	tests := []testCase{
+		{
+			handWithBid: []HandWithBid{
+				{
+					hand: Hand{
+						cards:    "32T3K",
+						handType: OnePair,
+					},
+					bid: 765,
+				},
+				{
+					hand: Hand{
+						cards:    "T55J5",
+						handType: ThreeOfAkind,
+					},
+					bid: 684,
+				},
+				{
+					hand: Hand{
+						cards:    "KK677",
+						handType: TwoPair,
+					},
+					bid: 28,
+				},
+				{
+					hand: Hand{
+						cards:    "KTJJT",
+						handType: TwoPair,
+					},
+					bid: 220,
+				},
+				{
+					hand: Hand{
+						cards:    "QQQJA",
+						handType: ThreeOfAkind,
+					},
+					bid: 483,
+				},
+			},
+			wantHandWithBid: []HandWithBid{
+				{
+					hand: Hand{
+						cards:    "32T3K",
+						handType: OnePair,
+					},
+					bid: 765,
+				},
+				{
+					hand: Hand{
+						cards:    "KTJJT",
+						handType: TwoPair,
+					},
+					bid: 220,
+				},
+				{
+					hand: Hand{
+						cards:    "KK677",
+						handType: TwoPair,
+					},
+					bid: 28,
+				},
+				{
+					hand: Hand{
+						cards:    "T55J5",
+						handType: ThreeOfAkind,
+					},
+					bid: 684,
+				},
+				{
+					hand: Hand{
+						cards:    "QQQJA",
+						handType: ThreeOfAkind,
+					},
+					bid: 483,
+				},
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			sortByStrength(tt.handWithBid)
+			if !reflect.DeepEqual(tt.wantHandWithBid, tt.handWithBid) {
+				t.Errorf("expected %v, but got %v", tt.wantHandWithBid, tt.handWithBid)
+			}
+		})
+	}
+}
